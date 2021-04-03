6c250216f34b9a8d9d20c48f795ea4a9dfc30fe3
Adam Olson <adamlsn@gmail.com>
2021-04-03 11:30:56 -0500

    adding logic to save data to local storage

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 let presentDay = moment();
 $("#currentDay").text(presentDay.format("dddd" + ", " + "MMMM do YYYY"));
+
+$(document).ready(function() {
+    //adding event listeners for buttons
+    $(".saveBtn").on("click", function() {
+        let currentHour = $(this).parent().attr("id");
+        var currentEvent = $(this).siblings(".description").val();
+        localStorage.setItem(currentHour, currentEvent);
+    });
+});
