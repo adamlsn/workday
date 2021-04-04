c4f424bbd505379675bdd8a2675e95f847d4d09e
Adam Olson <adamlsn@gmail.com>
2021-04-04 10:58:02 -0500

    changed staging of js variables, renamed "ids"

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-let presentDay = moment();
-$("#currentDay").text(presentDay.format("dddd" + ", " + "MMMM do YYYY"));
-
 $(document).ready(function() {
+    //setting "global variables"
+    let presentDay = moment();
+    $("#currentDay").text(presentDay.format("dddd" + ", " + "MMMM do YYYY"));
+
     //adding event listeners for buttons
     $(".saveBtn").on("click", function() {
         let currentHour = $(this).parent().attr("id");
-        var currentEvent = $(this).siblings(".description").val();
+        let currentEvent = $(this).siblings(".description").val();
         localStorage.setItem(currentHour, currentEvent);
     });
+
+    //getting data to display on page after refresh
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
 });
