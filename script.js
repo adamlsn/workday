900dadf34a680f880ef8b2460cea660611f65c4a
Adam Olson <adamlsn@gmail.com>
2021-04-04 13:31:33 -0500

    adding more "ids" to HTML, changing "ids" in js and HTML"

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ $(document).ready(function() {
     });
 
     //getting data to display on page after refresh
-    $("#9 .description").val(localStorage.getItem("9"));
-    $("#10 .description").val(localStorage.getItem("10"));
-    $("#11 .description").val(localStorage.getItem("11"));
-    $("#12 .description").val(localStorage.getItem("12"));
-    $("#13 .description").val(localStorage.getItem("13"));
-    $("#14 .description").val(localStorage.getItem("14"));
-    $("#15 .description").val(localStorage.getItem("15"));
-    $("#16 .description").val(localStorage.getItem("16"));
-    $("#17 .description").val(localStorage.getItem("17"));
+    $("#9a .description").val(localStorage.getItem("9a"));
+    $("#10a .description").val(localStorage.getItem("10a"));
+    $("#11a .description").val(localStorage.getItem("11a"));
+    $("#12p .description").val(localStorage.getItem("12p"));
+    $("#1p .description").val(localStorage.getItem("1p"));
+    $("#2p .description").val(localStorage.getItem("2p"));
+    $("#3p .description").val(localStorage.getItem("3p"));
+    $("#4p .description").val(localStorage.getItem("4p"));
+    $("#5p .description").val(localStorage.getItem("5p"));
 });
